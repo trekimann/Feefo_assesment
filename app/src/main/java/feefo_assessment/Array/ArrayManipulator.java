4a167999635f0c0c269821ca9d8c4c072aaabd54
trekimann <10469075+trekimann@users.noreply.github.com>
2022-05-19 13:12:49 +0100

    first pass at array stats

--- app/src/main/java/feefo_assessment/Array/ArrayManipulator.java
+++ app/src/main/java/feefo_assessment/Array/ArrayManipulator.java
@@ -1,7 +1,36 @@
 package feefo_assessment.Array;
 
+import java.util.Arrays;
+
 public class ArrayManipulator {
-    public void getStats(int[] input){
+    public ArrayStats getStats(int[] input){
+        ArrayStats arrayStats = new ArrayStats();
         
+        // sort the array
+        Arrays.sort(input);
+        int inputLength = input.length;
+        // Median is the middle number of a set, so just get the middle number in the array
+        int middleOfArray = (int) Math.ceil(inputLength - 1/2); // might be a fraction so round up
+        arrayStats.setMedian(input[middleOfArray]);
+        
+        // Mean is the sum of a set divided by the number in the set
+        int sum = 0;
+
+        for (int i : input) {
+            sum = sum + i;
+        }
+        arrayStats.setMean(sum / inputLength);
+           
+        // Mode of a set is the most frequent number in that set so group the array by unique occurences, which ever is the least unique is the Mode.
+        
+        // 
+        
+        // Range of a set is the difference between the smallest and highest numbers in the set. Subtract the first element of the array from the last.
+        arrayStats.setRange(input[inputLength+1] - input[0]);
+       
+        //print stats
+        arrayStats.printStats();
+
+        return arrayStats;
     }
 }
