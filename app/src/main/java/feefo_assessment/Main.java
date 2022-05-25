ffe99a4502c255fcb6823372844354ffae2472d7
trekimann <10469075+trekimann@users.noreply.github.com>
2022-05-25 21:58:49 +0100

    format doc

--- app/src/main/java/feefo_assessment/Main.java
+++ app/src/main/java/feefo_assessment/Main.java
@@ -9,42 +9,42 @@ public class Main {
     public static void main(String[] args) {
 
         // 1.1 Standardizing Job Titles process
-        
+
         String jt = "Java engineer";
         JobStandardizer s = new JobStandardizer();
         String standardizedTitle = s.standardize(jt);
-        //output standardizedTitle 
+        // output standardizedTitle
 
         jt = "C# engineer";
         standardizedTitle = s.standardize(jt);
-        //output standardizedTitle
+        // output standardizedTitle
 
         jt = "Chief Accountant";
-        standardizedTitle = s.standardize(jt); 
-        //output standardizedTitle
+        standardizedTitle = s.standardize(jt);
+        // output standardizedTitle
 
 
         // 1.2 Number statistics
 
-        //load the results from a big computation into mybigarray
-        int[] mybigarray = generateSomeHugeArray(); 
-        //now print out some basic stats on the array
+        // load the results from a big computation into mybigarray
+        int[] mybigarray = generateSomeHugeArray();
+        // now print out some basic stats on the array
         ArrayManipulator arrayManipulator = new ArrayManipulator();
         arrayManipulator.getStats(mybigarray);
     }
 
-    public static int[] generateSomeHugeArray(){
+    public static int[] generateSomeHugeArray() {
         Random random = new Random();
-        
+
         // how many numbers in array?
         int minSizeOfArray = 1000;
         int maxSizeOfArray = 10000;
-        int numberOfNumbers = random.nextInt(maxSizeOfArray-minSizeOfArray)+minSizeOfArray;
+        int numberOfNumbers = random.nextInt(maxSizeOfArray - minSizeOfArray) + minSizeOfArray;
 
         // populate array
         int[] hugeArray = new int[numberOfNumbers];
-        
-        for(int i = 0;i<numberOfNumbers; i++){
+
+        for (int i = 0; i < numberOfNumbers; i++) {
             hugeArray[i] = random.nextInt(maxSizeOfArray);
         }
 
